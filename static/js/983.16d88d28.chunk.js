bb112a10a8a7c3263550b6bf66666b129376d09d
iq-dev-expert <117043636+iq-dev-expert@users.noreply.github.com>
2023-10-21 20:31:52 +0000

    Deploying to gh-pages from @ iq-dev-expert/car-rent@004667c42e03ac4faf8609ac768df10f571e9586 🚀

---
+++ static/js/983.16d88d28.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[983],{983:function(r,t,e){e.r(t),e.d(t,{default:function(){return c}});var n=e(87),a=e(184);function c(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(n.rU,{to:"/catalog",children:"Catalog"}),(0,a.jsx)("br",{}),(0,a.jsx)(n.rU,{to:"/favorites",children:"Favorites"})]})}}}]);
+//# sourceMappingURL=983.16d88d28.chunk.js.map
