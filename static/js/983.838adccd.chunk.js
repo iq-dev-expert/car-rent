d1a1295deeb1dbde3ed9f4009cbd0e7d64e323ec
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-21 17:51:25 +0200

    Updates

---
+++ static/js/983.838adccd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[983],{983:function(e,n,r){r.r(n),r.d(n,{default:function(){return u}});var t=r(184);function u(){return(0,t.jsx)("h1",{children:"Not More HomePage"})}}}]);
+//# sourceMappingURL=983.838adccd.chunk.js.map
