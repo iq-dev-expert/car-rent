b13602d42a1976f75579200e851a934ffd76f21d
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-21 17:22:24 +0200

    add start settings and routes

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { lazy } from 'react';
+
+import { Layout } from './Layout/Layout';
+
+const HomePage = lazy(() => import('pages/HomePage'));
+const CatalogPage = lazy(() => import('pages/CatalogPage'));
+const FavoritesPage = lazy(() => import('pages/FavoritesPage'));
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
+};
