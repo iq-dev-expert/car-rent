14e3d6549ad1edf3b70bd93f23ec4fc9c5ae7cc8
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-10-22 21:39:36 +0200

    add global slice error and isloading

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { carsReducer, carsSlice } from './reduxCarsList/carsSlise';
 
-const rootReduser = combineReducers({ [carsSlice.name]: carsReducer });
+import { carsReducer, carsSlice } from './reduxCarsList/carsSlise';
+import { globalReducer, globalSlice } from './reduxGlobal/globalSlice';
+
+const rootReduser = combineReducers({
+  [carsSlice.name]: carsReducer,
+  [globalSlice.name]: globalReducer,
+});
 
 export const store = configureStore({
   reducer: rootReduser,
