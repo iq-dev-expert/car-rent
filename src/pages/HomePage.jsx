850a5fbda1c0e3ec1f68a5d545b1ce72bff8ea37
Oleksandr Rakomin <uamilliardua@gmail.com>
2023-11-19 10:57:18 +0100

    test

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 export default function HomePage() {
   return (
     <>
+      <h1>Hello world</h1>
       <Link to="/catalog">Catalog</Link>
       <br />
       <Link to="/favorites">Favorites</Link>
